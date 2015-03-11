bd2f5d3fea69a7825f1353ee38330fa124dd3f1a
Marius Mandal <mariusmandal@gmail.com>
2015-03-11 17:07:04 +0100

    Vis boksen 2 uker før oppstart, ikke 30 dager

--- controller/dashboard.controller.php
+++ controller/dashboard.controller.php
@@ -2,7 +2,7 @@
 require_once('UKM/monstring.class.php');
 $monstring = new monstring( get_option('pl_id') );
 
-$show_box = $monstring->g('pl_start') - (30*24*3600);
+$show_box = $monstring->g('pl_start') - (14*24*3600);
 
 if( time() > $show_box && !$monstring->ferdig() ) {
 	require_once(dirname(__FILE__).'/push_to_front.controller.php');
