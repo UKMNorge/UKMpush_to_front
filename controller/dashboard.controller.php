dd30d19b32b0029bda2c07840bdfcdc16e4333d2
Asgeir Stavik Hustad <askhus@online.no>
2016-03-01 17:08:36 +0000

    Fiks opp dashboard-view.

--- controller/dashboard.controller.php
+++ controller/dashboard.controller.php
@@ -8,5 +8,16 @@ if( time() > $show_box && !$monstring->ferdig() ) {
 	require_once(dirname(__FILE__).'/push_to_front.controller.php');
 	
 	$dashboardPTF = TWIG('wp_dashboard.twig.html', $TWIGdata, dirname(dirname(__FILE__)));
+	// FETCH POSTS
+	$posts = query_posts( 'post_status=publish&posts_per_page=-1' );
+	global $post;
+	foreach( $posts as $key => $post ) {
+		the_post();
+		$TWIGdata['my_posts'][] = new WPOO_Post( $post );
+	}
+	// Load data for selectlist default value
+	for( $i=1; $i<4; $i++ ) {
+		$TWIGdata['UKMpush_to_front_post'][$i] = (int) get_option('UKMpush_to_front_post_'.$i);
+	}
 	$TWIGdata['block_pre_messages'][] = $dashboardPTF;
 }
